06c7ad696c4fe5044af33e48046d2c788b4e4d04
ieguiguren <donosor00@gmail.com>
2016-01-11 01:31:44 +0100

    first commit

---
+++ caesar.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# CAESAR Cipher
+
+shift = 13
+string = 'Gur cnffjbeq vf 5Gr8L4qetPEsPk8htqjhRK8XSP6x2RHh'
+
+l = 'abcdefghijklmnopqrstuvwxyz' * 2
+L = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' * 2
+
+result = ''
+
+for i in s:
+     if i.isupper():
+        a += LL[L.index(i) + 13]
+     elif i.islower():
+         a += ll[l.index(i) + 13]
+     else:
+         a += i
+''' print a
+'The password is 5Te8Y4drgCRfCx8ugdwuEX8KFC6k2EUu'
+'''
+
